dadbbd4b1db8dc0a70dd184d7a66837eb3b0e968
panicbus <elementalair@gmail.com>
2016-06-08 10:12:17 -0700

    adding stylus and ejs

--- app.js
+++ app.js
@@ -5,26 +5,57 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var ejs = require('ejs');
+var nodemon = require('nodemon');
+var favicon = require('serve-favicon');
+var stylus = require('stylus');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+
+
+// for views
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/assets', express.static(__dirname + '/public'));
+// bring in the public directory -- remember to require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 // uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(stylus.middleware({src: path.join(__dirname, 'public'), compress: true}));
+
+// app.use(stylus.middleware({
+//   src: __dirname + '/public',
+//   dest: __dirname + '/public'
+// }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// render the main page
+// app.get('/', function(req, res){
+//   res.render('index');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,5 +88,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(port);
 
 module.exports = app;
