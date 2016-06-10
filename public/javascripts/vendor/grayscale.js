802a7ab5952174de62fadcedf06ea5ba499ab8e1
panicbus <elementalair@gmail.com>
2016-06-10 11:45:45 -0700

    splash page, nav

--- public/javascripts/vendor/grayscale.js
+++ public/javascripts/vendor/grayscale.js
@@ -4,27 +4,31 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
+// for navbar change on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+	if ($('.navbar').offset().top > 50) {
+		$('.navbar-fixed-top').addClass('top-nav-collapse');
+		$('.logo').css('display', 'none');
+		$('.nav-logo').css('display', 'block');
+	} else {
+		$('.navbar-fixed-top').removeClass('top-nav-collapse');
+		$('.logo').css('display', 'block');
+		$('.nav-logo').css('display', 'none');
+	}
 });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+// for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
 });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+	$('.navbar-toggle:visible').click();
 });
