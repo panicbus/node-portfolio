e96ed9732404bb171abe1cd6b530b4c8d68ee93d
panicbus <elementalair@gmail.com>
2016-06-10 13:53:49 -0700

    scroll to and on scroll highlighting

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,63 @@
- $(document).ready(function() {
+$(document).ready(function() {
+
+ 	// for navbar change on scroll
+	$(window).scroll(function() {
+		if ($('.navbar').offset().top > 50) {
+			$('.navbar-fixed-top').addClass('top-nav-collapse');
+			$('.logo').css('display', 'none');
+			$('.nav-logo').css('display', 'block');
+		} else {
+			$('.navbar-fixed-top').removeClass('top-nav-collapse');
+			$('.logo').css('display', 'block');
+			$('.nav-logo').css('display', 'none');
+		}
+	});
+
+	// for page scroll to feature - uses jQuery Easing plugin
+	// http://gsgd.co.uk/sandbox/jquery/easing/jquery.easing.1.3.js
+	$(function() {
+		$('a.page-scroll').bind('click', function(event) {
+			event.preventDefault();
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+		});
+	});
+
+	// tab highlighting on section scroll
+  $(document).scroll(function(){
+
+    var pageScroll = $(this).scrollTop();
+    var aboutScroll = $('#about').offset().top - 100;
+    var portfolioScroll = $('#portfolio').offset().top - 100;
+    var contactScroll = $('#contact').offset().top - 100;
+
+    if (pageScroll <= aboutScroll ) {
+      $('.page-scroll').removeClass('active');
+      $('.work-scroll').addClass('active');
+    }
+    if (pageScroll > aboutScroll && pageScroll <= portfolioScroll ) {
+      $('.page-scroll').removeClass('active');
+      $('.about-scroll').addClass('active');
+    }
+    if (pageScroll > portfolioScroll && pageScroll <= contactScroll ) {
+      $('.page-scroll').removeClass('active');
+      $('.portfolio-scroll').addClass('active');
+    }
+    if (pageScroll > contactScroll ) {
+      $('.page-scroll').removeClass('active');
+      $('.contact-scroll').addClass('active');
+    }
+  })
+
+
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function() {
+		$('.navbar-toggle:visible').click();
+	});
+
+
 
 	$('#owl-demo').owlCarousel({
 	    autoPlay: 3000, //Set AutoPlay to 3 seconds
--- public/javascripts/vendor/grayscale.js
+++ public/javascripts/vendor/grayscale.js
@@ -4,31 +4,3 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// for navbar change on scroll
-$(window).scroll(function() {
-	if ($('.navbar').offset().top > 50) {
-		$('.navbar-fixed-top').addClass('top-nav-collapse');
-		$('.logo').css('display', 'none');
-		$('.nav-logo').css('display', 'block');
-	} else {
-		$('.navbar-fixed-top').removeClass('top-nav-collapse');
-		$('.logo').css('display', 'block');
-		$('.nav-logo').css('display', 'none');
-	}
-});
-
-// for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-	$('a.page-scroll').bind('click', function(event) {
-		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500, 'easeInOutExpo');
-		event.preventDefault();
-	});
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-	$('.navbar-toggle:visible').click();
-});
