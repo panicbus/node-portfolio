2026b6e196e3dc804a355ee6c64e86d7f2bb8832
panicbus <elementalair@gmail.com>
2016-06-14 16:24:40 -0700

    projects & projectImages

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -64,6 +64,7 @@ $(document).ready(function() {
     $(this).css('background', getRandomColor());
   })
 
+
 	// Closes the Responsive Menu on Menu Item Click
 	$('.navbar-collapse ul li a').click(function() {
 		$('.navbar-toggle:visible').click();
