e8ade1430a3088363647c3801b3e2d499dc86574
Nico Crisafulli <ncrisafulli@knowledgepath.com>
2020-08-09 23:57:16 -0700

    optimizing scrollmagic for mobile, removing unness images, new bio pic

--- public/javascripts/vendor/scrollMagic.js
+++ public/javascripts/vendor/scrollMagic.js
@@ -1,4 +1,37 @@
-function splitScroll(){
+// function splitScroll(){
+// 	const controller = new ScrollMagic.Controller();
+
+// 	var introHeight = $('#intro').height(), 
+// 			spaceHeight = $('#spacey').height(), 
+// 			skillsHeight = $('#skills').height(),
+// 			portfolioHeight = $('#portfolio').height(),
+// 			jobHeight = $('#job').height(),
+// 			leftSide = $('.container__left').height();
+	
+// 	var elementHeight = introHeight + spaceHeight + skillsHeight + portfolioHeight + jobHeight - leftSide;
+
+// 	new ScrollMagic.Scene({
+// 		duration: elementHeight,
+// 		triggerElement: '.split-scroll',
+// 		triggerHook: 0
+// 	})
+// 	.setPin('.split-scroll')
+// 	// .addIndicators()
+// 	.addTo(controller);
+// }
+// splitScroll();
+
+
+var tooSmall = false;
+var controller = null;
+var maxWidth = 959;
+
+if( $(window).width() < maxWidth ) {
+	tooSmall = true;
+}
+
+function initScrollMagic() {
+	$('body').scrollTop( 0 );
 	const controller = new ScrollMagic.Controller();
 
 	var introHeight = $('#intro').height(), 
@@ -19,4 +52,25 @@ function splitScroll(){
 	// .addIndicators()
 	.addTo(controller);
 }
-splitScroll();
+
+if( !tooSmall ) {
+	initScrollMagic();
+}
+
+// part of the problem is that window resizes can trigger multiple times as the events fire rapidly
+// this solution prevents the controller from being initialized/destroyed more than once
+$(window).resize( function() {
+	var wWidth = $(window).width();
+	if( wWidth < maxWidth ) {
+		if( controller !== null && controller !== undefined ) {
+			// completely destroy the controller
+			controller = controller.destroy( true );
+			// if needed, use jQuery to manually remove styles added to DOM elements by GSAP etc. here
+		}
+	} else if( wWidth >= maxWidth ) {
+		if( controller === null || controller === undefined ) {
+			// reinitialize ScrollMagic only if it is not already initialized
+			initScrollMagic();
+		}
+	}
+});
